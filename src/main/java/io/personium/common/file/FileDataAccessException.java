5e8345596d69c4c79e9367280a5f2ba920958ff9
akioshimono <akio@shimono.net>
2019-10-25 16:02:23 +0900

    minor change

--- src/main/java/io/personium/common/file/FileDataAccessException.java
+++ src/main/java/io/personium/common/file/FileDataAccessException.java
@@ -2,7 +2,6 @@
  * Personium
  * Copyright 2014-2019 Personium Project
  * - FUJITSU LIMITED
-
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
