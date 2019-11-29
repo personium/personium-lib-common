d739798bfacb494387a57ea5b1606894d6a06db7
akioshimono <akio@shimono.net>
2019-11-29 20:29:39 +0900

    add a test to parse and create a VisiorRefreshToken. change dubious mocking tests to minimize the use of mocking. Probably, the target method itself (refreshAccessToken) is useless. It can easily be substituted with AccessToken Constructor.

--- src/test/java/io/personium/common/auth/token/VisitorRefreshTokenTest.java
+++ src/test/java/io/personium/common/auth/token/VisitorRefreshTokenTest.java
@@ -1,6 +1,7 @@
 /**
- * personium.io
- * Copyright 2014 FUJITSU LIMITED
+ * Personium
+ * Copyright 2014 - 2019 Personium Project Authors
+ *  - FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,84 +19,118 @@ package io.personium.common.auth.token;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Unit Test class for CellLocalRefreshToken.
+ * Unit Test class for VisitorRefreshTokenTest.
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({VisitorRefreshToken.class})
-@PowerMockIgnore({ "javax.xml.crypto.dsig.*", "javax.security.auth.*" })
 public class VisitorRefreshTokenTest {
+    static Logger log = LoggerFactory.getLogger(VisitorRefreshTokenTest.class);
+
+    static final Long ISSUED_AT = new Date().getTime();
+    static final Long LIFESPAN = AbstractOAuth2Token.ACCESS_TOKEN_EXPIRES_MILLISECS;
+
+    static final String ISSUER = "https://issuer.localhost/";
+    static final String SUBJECT = "https://subject.localhost/#acc";
+    static String TARGET = "https://target.localhost/";
+    static String SCHEMA = "https://schema.localhost/";
+    static String[] SCOPE = new String[] {"auth", "message-read"};
+    static List<Role> ROLE_LIST = new ArrayList<>();
+    static Set<String> SCOPE_SET = new HashSet<>();
+    static {
+        ROLE_LIST.add(new Role("role1", "box", "https://schema.localhost/", "https://schema.localhost/"));
+        ROLE_LIST.add(new Role("role2", "box", "https://schema.localhost/", "https://subject.localhost/"));
+    }
+
 
     /** Target class of unit test. */
-    private VisitorRefreshToken transCellRefreshToken;
+    private VisitorRefreshToken visitorRefreshToken;
+
+    @BeforeClass
+    public static void beforeClass() {
+        AbstractLocalToken.setKeyString("a123456789abcdef");
+    }
 
     /**
      * Before.
      */
     @Before
-    public void befor() {
-        transCellRefreshToken = PowerMockito.spy(new VisitorRefreshToken(null, null, null, null, null, null, null));
+    public void before() {
+        visitorRefreshToken = new VisitorRefreshToken(
+                "12345",
+                ISSUED_AT,
+                LIFESPAN,
+                ISSUER,
+                SUBJECT,
+                ISSUER,
+                ROLE_LIST,
+                SCHEMA,
+                SCOPE);
     }
 
     /**
      * Test refreshAccessToken().
      */
-    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Test
     public void refreshAccessToken_Normal() {
         // --------------------
         // Test method args
         // --------------------
-        long issuedAt = 1L;
-        String target = "https://personium/targetcell/";
-        String cellUrl = "https://personium/testcell/";
-        List<Role> roleList = new ArrayList<>();
-        Role role = new Role("roleName");
-        roleList.add(role);
+//        long issuedAt = 1L;
+//        String target = "https://personium/targetcell/";
+//        String cellUrl = "https://personium/testcell/";
+//        List<Role> roleList = new ArrayList<>();
+//        Role role = new Role("roleName");
+//        roleList.add(role);
 
         // --------------------
         // Mock settings
         // --------------------
-        IAccessToken ret = new ResidentLocalAccessToken(1L, null, null, null, null);
-        ArgumentCaptor<Long> issuedAtCaptor = ArgumentCaptor.forClass(Long.class);
-        ArgumentCaptor<String> targetCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<String> cellUrlCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<List> roleListCaptor = ArgumentCaptor.forClass(List.class);
-        PowerMockito.doReturn(ret).when(transCellRefreshToken).refreshAccessToken(
-                issuedAtCaptor.capture(), targetCaptor.capture(), cellUrlCaptor.capture(),
-                roleListCaptor.capture());
+
+//        IAccessToken ret = new ResidentLocalAccessToken(1L, null, null, null, null);
+//        ArgumentCaptor<Long> issuedAtCaptor = ArgumentCaptor.forClass(Long.class);
+//        ArgumentCaptor<String> targetCaptor = ArgumentCaptor.forClass(String.class);
+//        ArgumentCaptor<String> cellUrlCaptor = ArgumentCaptor.forClass(String.class);
+//        ArgumentCaptor<List> roleListCaptor = ArgumentCaptor.forClass(List.class);
+//        Mockito.doReturn(ret).when(visitorRefreshToken).refreshAccessToken(
+//                issuedAtCaptor.capture(), targetCaptor.capture(), cellUrlCaptor.capture(),
+//                roleListCaptor.capture());
 
         // --------------------
         // Expected result
         // --------------------
         // Nothing.
+        Long refreshedAt = ISSUED_AT + LIFESPAN * 2;
 
         // --------------------
         // Run method
         // --------------------
-        transCellRefreshToken.refreshAccessToken(issuedAt, target, cellUrl, roleList);
+        VisitorLocalAccessToken at = (VisitorLocalAccessToken)visitorRefreshToken
+                .refreshAccessToken(refreshedAt, LIFESPAN, null, ISSUER, ROLE_LIST);
 
         // --------------------
         // Confirm result
         // --------------------
-        assertThat(issuedAtCaptor.getValue(), is(issuedAt));
-        assertThat(targetCaptor.getValue(), is(target));
-        assertThat(cellUrlCaptor.getValue(), is(cellUrl));
-        assertThat(roleListCaptor.getValue(), is(roleList));
+        assertEquals(refreshedAt, Long.valueOf(at.issuedAt));
+        assertEquals(ISSUER, at.getIssuer());
+        assertEquals(SUBJECT, at.getSubject());
+        assertEquals(SCHEMA, at.getSchema());
+        assertTrue(Arrays.deepEquals(ROLE_LIST.toArray(), at.getRoleList().toArray()));
     }
 
     /**
@@ -105,47 +140,28 @@ public class VisitorRefreshTokenTest {
      */
     @Test
     public void refreshAccessToken_Normal_schema_is_null_target_is_null() {
-        transCellRefreshToken = PowerMockito.spy(new VisitorRefreshToken(
-                null, null, "https://personium/subject/", null, null, "https://personium/schema/", null));
         // --------------------
         // Test method args
         // --------------------
-        long issuedAt = 1L;
-        String target = null;
-        String cellUrl = "https://personium/testcell02/";
-        List<Role> roleList = new ArrayList<>();
-        Role role = new Role("roleName");
-        roleList.add(role);
 
-        // --------------------
-        // Mock settings
-        // --------------------
-        // Nothing.
 
-        // --------------------
-        // Expected result
-        // --------------------
-        Long expectedIssuedAt = issuedAt;
-        String expectedIssuer = cellUrl;
-        String expectedSubject = "https://personium/subject/";
-        List<Role> expectedRoleList = roleList;
-        String expectedSchema = "https://personium/schema/";
+        Long refreshedAt = ISSUED_AT + LIFESPAN * 2;
 
         // --------------------
         // Run method
         // --------------------
-        IAccessToken actual = transCellRefreshToken.refreshAccessToken(issuedAt, target, cellUrl, roleList);
+        IAccessToken actual = visitorRefreshToken.refreshAccessToken(refreshedAt, LIFESPAN, null, ISSUER, ROLE_LIST);
 
         // --------------------
         // Confirm result
         // --------------------
         assertThat(actual, instanceOf(VisitorLocalAccessToken.class));
         VisitorLocalAccessToken castActual = (VisitorLocalAccessToken) actual;
-        assertThat(castActual.issuedAt, is(expectedIssuedAt));
-        assertThat(castActual.getIssuer(), is(expectedIssuer));
-        assertThat(castActual.getSubject(), is(expectedSubject));
-        assertThat(castActual.getRoleList(), is(expectedRoleList));
-        assertThat(castActual.getSchema(), is(expectedSchema));
+        assertEquals(refreshedAt, Long.valueOf(castActual.issuedAt));
+        assertEquals(ISSUER, castActual.getIssuer());
+        assertEquals(SUBJECT, castActual.getSubject());
+        assertEquals(SCHEMA, castActual.getSchema());
+        assertTrue(Arrays.deepEquals(ROLE_LIST.toArray(), castActual.getRoleList().toArray()));
     }
 
     /**
@@ -159,17 +175,17 @@ public class VisitorRefreshTokenTest {
         // --------------------
         // Test method args
         // --------------------
-        long issuedAt = 1L;
-        String subject = "https://personium/subject/";
-        String target = "https://personium/targetcell/";
-        String cellUrl = "https://personium/testcell02/";
-        List<Role> roleList = new ArrayList<>();
-        Role role = new Role("roleName");
-        roleList.add(role);
-        String schema = "https://personium/appcell/";
+//        long issuedAt = 1L;
+//        String subject = "https://personium/subject/";
+//        String target = "https://personium/targetcell/";
+//        String cellUrl = "https://personium/testcell02/";
+//        List<Role> roleList = new ArrayList<>();
+//        Role role = new Role("roleName");
+//        roleList.add(role);
+//        String schema = "https://personium/appcell/";
 
-        transCellRefreshToken = PowerMockito.spy(new VisitorRefreshToken(
-                null, null, subject, null, null, schema, null));
+//        visitorRefreshToken = new VisitorRefreshToken(
+//                null, null, subject, null, null, schema, null);
 
         // X509 settings.
         String folderPath = "x509/effective/";
@@ -188,28 +204,54 @@ public class VisitorRefreshTokenTest {
         // --------------------
         // Expected result
         // --------------------
-        Long expectedIssuedAt = issuedAt;
-        String expectedIssuer = cellUrl;
-        String expectedTarget = target;
-        List<Role> expectedRoleList = roleList;
-        String expectedSchema = schema;
-        String expectedSubject = subject;
+        Long refreshedAt = ISSUED_AT + LIFESPAN * 2;
 
         // --------------------
         // Run method
         // --------------------
-        IAccessToken actual = transCellRefreshToken.refreshAccessToken(issuedAt, target, cellUrl, roleList);
+        IAccessToken actual = visitorRefreshToken.refreshAccessToken(refreshedAt,LIFESPAN, TARGET, ISSUER, ROLE_LIST);
 
         // --------------------
         // Confirm result
         // --------------------
         assertThat(actual, instanceOf(TransCellAccessToken.class));
         TransCellAccessToken castActual = (TransCellAccessToken) actual;
-        assertThat(castActual.issuedAt, is(expectedIssuedAt));
-        assertThat(castActual.getIssuer(), is(expectedIssuer));
-        assertThat(castActual.getSubject(), is(expectedSubject));
-        assertThat(castActual.getTarget(), is(expectedTarget));
-        assertThat(castActual.getRoleList(), is(expectedRoleList));
-        assertThat(castActual.getSchema(), is(expectedSchema));
+        assertThat(castActual.issuedAt, is(refreshedAt));
+        assertThat(castActual.getIssuer(), is(ISSUER));
+        assertThat(castActual.getSubject(), is(SUBJECT));
+        assertThat(castActual.getTarget(), is(TARGET));
+        assertThat(castActual.getRoleList(), is(ROLE_LIST));
+        assertThat(castActual.getSchema(), is(SCHEMA));
+    }
+    @Test
+    public void parse_ParsedRoles_ShouldBe_SameAs_Original() throws Exception {
+        VisitorRefreshToken vrt = new VisitorRefreshToken(
+                "12345",
+                new Date().getTime(),
+                AbstractOAuth2Token.ACCESS_TOKEN_EXPIRES_MILLISECS,
+                ISSUER,
+                SUBJECT,
+                ISSUER,
+                ROLE_LIST,
+                SCHEMA,
+                SCOPE);
+        String tokenStr = vrt.toTokenString();
+        VisitorRefreshToken parsedToken = VisitorRefreshToken.parse(tokenStr, ISSUER);
+        List<Role> parsedRoles = parsedToken.getRoleList();
+        // Parsed roles should be kept the same.
+        assertEquals(ROLE_LIST.size(), parsedRoles.size());
+        StringBuilder sb1 = new StringBuilder();
+        for (Role role : ROLE_LIST) {
+            sb1.append(role.toRoleClassURL());
+            sb1.append(" ");
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for (Role role : parsedRoles) {
+            sb2.append(role.toRoleClassURL());
+            sb2.append(" ");
+        }
+        log.info(sb1.toString());
+        log.info(sb2.toString());
+        assertEquals(sb1.toString(), sb2.toString());
     }
 }
